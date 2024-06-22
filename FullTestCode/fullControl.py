bf98484f30eb3aeb1df9bdd3d88c59d35bce7a80
NWarren42 <noahwarren42@gmail.com>
2024-06-22 18:47:01 -0400

    Start function type hinting

--- FullTestCode/fullControl.py
+++ FullTestCode/fullControl.py
@@ -1,20 +1,31 @@
-from labjack import ljm
-from QDAC_Class import *
-import time
-import matplotlib.pyplot as plt
-import json
 import csv
+import json
 import msvcrt
+import time
+from typing import TYPE_CHECKING
 
-def jsonDefineIO(handle, configFilename):
-    '''
-    This function creates the objects for the desired test setup from a JSON file following the format
-    specified in /configFramework.json.
-    '''
+from labjack import ljm
+from QDAC_Class import LoadCell, PressureTransducer, Thermocouple, Valve
 
-    with open(configFilename, 'r') as f:
+if TYPE_CHECKING:
+    T_sensorTypes = Thermocouple | PressureTransducer | LoadCell
+
+def jsonDefineIO(handle: int,
+                 configFilename: str,
+                 ) -> tuple[dict[str, T_sensorTypes],
+                            dict[str, Valve],
+                            str,
+                            str]:
+    """Create objects for the desired test setup from a JSON file.
+
+    This function reads a JSON file specified by 'configFilename' to create objects
+    representing the desired test setup. The JSON file should follow the format
+    defined in '/configFramework.json'.
+    """
+
+    with open(configFilename, "r") as f:
         config = json.load(f)
-    
+
     sensorsObjects = {}
     valveObjects = {}
 
@@ -22,61 +33,61 @@ def jsonDefineIO(handle, configFilename):
         for sensorName, sensorInfo in sensors.items():
             if sensorType == "thermocouple":
                 pin = sensorInfo["pin"]
-                sensorsObjects[sensorName] = thermocouple(handle, pin)
-            
+                sensorsObjects[sensorName] = Thermocouple(handle, pin)
+
             elif sensorType == "pressureTransducer":
                 pin = sensorInfo["pin"]
                 pressureRange = sensorInfo["maxPressure_PSI"]
-                sensorsObjects[sensorName] = pressureTransducer(handle, pin, pressureRange)
-            
+                sensorsObjects[sensorName] = PressureTransducer(handle, pin, pressureRange)
+
             elif sensorType == "loadCell":
                 negPin = sensorInfo["oddNegPin"]
                 posPin = sensorInfo["evenPosPin"]
                 maxWeight = sensorInfo["loadRating_N"]
                 excitation = sensorInfo["excitation_V"]
                 sensitivity = sensorInfo["sensitivity_vV"]
-                sensorsObjects[sensorName] = loadCell(handle, posPin, negPin, maxWeight, excitation, sensitivity)
+                sensorsObjects[sensorName] = LoadCell(handle, posPin, negPin, maxWeight, excitation, sensitivity)
 
     for valveName, valveInfo in config["valves"].items():
         pin = valveInfo["controlPin"]
         default = valveInfo["defaultState"]
-        valveObjects[valveName] = valve(handle, pin, default, valveName)
+        valveObjects[valveName] = Valve(handle, pin, default, valveName)
 
     return sensorsObjects, valveObjects, config["configName"], config["filePath"]
 
 def takeAllData(sensors):
-    ''' Takes sensor dictionary'''
+    """Takes sensor dictionary"""
     for sensor in sensors.values(): sensor.takeData()
 
 def exportTestDataCSV(timeStamps, sensors, dataDir, configName, configPath):
-    
+
     # Setting CSV filename
-    localTime = time.strftime('%Y-%m-%d %H-%M-%S', time.localtime(time.time()))
-    csvFilename = configName + '---' + localTime
+    localTime = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime(time.time()))
+    csvFilename = configName + "---" + localTime
 
     # Setting CSV headers
-    csvData = [['Time'] + list(sensors.keys())]
-    
+    csvData = [["Time"] + list(sensors.keys())]
+
     # Assembling CSV rows for each sensor
     for i in range(len(timeStamps)):
         row = [timeStamps[i]]
         for sensor in sensors.values():
-            if type(sensor) == thermocouple:
-                row.append(sensor.data_celsius[i]) 
-            if type(sensor) == pressureTransducer:
-                row.append(sensor.data_PSI[i]) 
-            if type(sensor) == loadCell:
-                row.append(sensor.data_kg[i]) 
+            if type(sensor) == Thermocouple:
+                row.append(sensor.data_celsius[i])
+            if type(sensor) == PressureTransducer:
+                row.append(sensor.data_PSI[i])
+            if type(sensor) == LoadCell:
+                row.append(sensor.data_kg[i])
         csvData.append(row)
-    
+
     # Writing data to csv
-    with open(dataDir + csvFilename, 'w', newline='') as csvfile:
+    with open(dataDir + csvFilename, "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(["Config File Name:", configName])
         writer.writerow(["Config File Path:", configPath])
         writer.writerow(["Test Time:", localTime])
         writer.writerows(csvData)
-    
+
     print(f"Data saved to {dataDir + csvFilename}")
 
 
@@ -91,27 +102,27 @@ sensors, valves, configName, dataDirectory = jsonDefineIO(handle, configPath)
 print("Sensors Initialized.")
 
 openValveMap = {
-    '1': "AVFill",  # FIO0
-    '2': "AVDump",  # FIO1
-    '3': "AVRun",  # FIO2
-    '4': "AVN2Purge1",  # FIO3
-    '5': "AVN2Purge2"   # FIO4
+    "1": "AVFill",  # FIO0
+    "2": "AVDump",  # FIO1
+    "3": "AVRun",  # FIO2
+    "4": "AVN2Purge1",  # FIO3
+    "5": "AVN2Purge2",   # FIO4
 }
 
 closeValveMap = {
-    'q': "AVFill",  # FIO0
-    'w': "AVDump",  # FIO1
-    'e': "AVRun",  # FIO2
-    'r': "AVN2Purge1",  # FIO3
-    't': "AVN2Purge2"   # FIO4
+    "q": "AVFill",  # FIO0
+    "w": "AVDump",  # FIO1
+    "e": "AVRun",  # FIO2
+    "r": "AVN2Purge1",  # FIO3
+    "t": "AVN2Purge2",   # FIO4
 }
 
 getStateMap = {
-    'a': "AVFill",  # FIO0
-    's': "AVDump",  # FIO1
-    'd': "AVRun",  # FIO2
-    'f': "AVN2Purge1",  # FIO3
-    'g': "AVN2Purge2"   # FIO4
+    "a": "AVFill",  # FIO0
+    "s": "AVDump",  # FIO1
+    "d": "AVRun",  # FIO2
+    "f": "AVN2Purge1",  # FIO3
+    "g": "AVN2Purge2",   # FIO4
 }
 
 startTime = time.time()
@@ -123,7 +134,7 @@ print("Enter control keys now:")
 lastTime = time.time()
 while(True):
     currentTime = time.time()
-    
+
 
     if (currentTime - lastTime) > sampleSpacing_s:
         takeAllData(sensors)
@@ -131,12 +142,12 @@ while(True):
         count += 1
         if count % 10 == 0:
             print(f"NitrousFillKG: {sensors['LCNitrousFill'].data_kg[count-1]-0.6:3.1f}--ThrustKG: {sensors['LCThrust'].data_kg[count-1]:3.1f}--PTRunPSI: {sensors['PTRun'].data_PSI[count-1]:3.1f}-- PTPreInjectorPSI: {sensors['PTPreInjector'].data_PSI[count-1]:3.1f}-- PTN20Supply: {sensors['PTN20Supply'].data_PSI[count-1]:3.1f}")
-        
-        
-    if msvcrt.kbhit():
-        key = msvcrt.getch().decode('utf-8')
 
-        if key == '/':
+
+    if msvcrt.kbhit():
+        key = msvcrt.getch().decode("utf-8")
+
+        if key == "/":
             print("Closing...")
             ljm.close(handle)
             break
@@ -144,18 +155,18 @@ while(True):
         if key in openValveMap: # Open Select Valve
             valveName = openValveMap[key]
             valves[valveName].openValve()
-        
+
         if key in closeValveMap: # Close Select Valve
             valveName = closeValveMap[key]
             valves[valveName].closeValve()
-        
+
         if key in getStateMap: # Get select state
             valveName = getStateMap[key]
             state = valves[valveName].currentState
             if state == 1: print(f"{valveName} is open")
             if state == 0: print(f"{valveName} is closed")
-        
-        if key == 'c': # Close ALL valves
+
+        if key == "c": # Close ALL valves
             for tempValve in valves.items():
                 tempValve[1].closeValve()
 
